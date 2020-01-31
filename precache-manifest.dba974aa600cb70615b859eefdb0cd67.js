5da0d74f843d3f63f5411952882cb24a46f17db6
monicalaura <monicalauraburns@yahoo.com>
2020-01-31 11:35:06 +0200

    Updates

---
+++ precache-manifest.dba974aa600cb70615b859eefdb0cd67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729cf2313709a02f7a2c72fa25569327",
+    "url": "/monicalaura/my-markdown/index.html"
+  },
+  {
+    "revision": "50d1013ee2645c2845bd",
+    "url": "/monicalaura/my-markdown/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dc02f04c8c8c379385b1",
+    "url": "/monicalaura/my-markdown/static/css/main.aa85d060.chunk.css"
+  },
+  {
+    "revision": "50d1013ee2645c2845bd",
+    "url": "/monicalaura/my-markdown/static/js/2.994e91d2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monicalaura/my-markdown/static/js/2.994e91d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc02f04c8c8c379385b1",
+    "url": "/monicalaura/my-markdown/static/js/main.c0f1cbca.chunk.js"
+  },
+  {
+    "revision": "37d46ffba5cd6762faa3",
+    "url": "/monicalaura/my-markdown/static/js/runtime-main.7fa30940.js"
+  }
+]);
